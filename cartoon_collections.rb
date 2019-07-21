ff3660518d5116a2d261473893b708f1217b2961
Monique Vitche <mvitche7@gmail.com>
2019-07-21 21:35:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf)
+  names = []
+  dwarf.each_with_index do |name, index|
+    names << "#{index + 1}. #{name}"
+  end
+  puts names
 end
 
 def summon_captain_planet# code an argument here
